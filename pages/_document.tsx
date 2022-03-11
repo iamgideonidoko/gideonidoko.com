e760bebc6367740cfd198d0bdd14e456ede2c357
Gideon Idoko <iamgideonidoko@gmail.com>
2022-03-11 20:23:50 +0100

    Update site favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                     <link rel="manifest" href="/site.webmanifest" />
-                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#41abc9" />
-                    <meta name="msapplication-TileColor" content="#ffffff" />
-                    <meta name="theme-color" content="#ffffff"></meta>
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ffffff" />
 
                     <link rel="stylesheet" href="/assets/icon/neu.css" />
                 </Head>
