f7664f2ec09c6f6040ea487f53fcaa38472385c2
Gideon Idoko <iamgideonidoko@gmail.com>
2023-03-08 10:16:10 +0100

    feat: add a page redirector

--- helper.ts
+++ helper.ts
@@ -438,3 +438,10 @@ export const purifyHtml = (text: string): string => {
         ],
     });
 };
+
+export const isValidURL = (url: string): boolean => {
+    const res = url.match(
+        /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi,
+    );
+    return res !== null;
+};
---
+++ pages/r.tsx
@@ -0,0 +1,35 @@
+import { Fragment } from 'react';
+import { NextSeo } from 'next-seo';
+import { GetServerSideProps } from 'next';
+import { isValidURL } from '../helper';
+
+const Redirector = () => {
+    return (
+        <Fragment>
+            <NextSeo title="Redirect" noindex={true} nofollow={true} />
+        </Fragment>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    const { url } = query as { url: string };
+
+    // redirect to url if valid
+    if (url && isValidURL(url))
+        return {
+            redirect: {
+                permanent: false,
+                destination: url,
+            },
+        };
+
+    // redirect to homepage if url is not valid
+    return {
+        redirect: {
+            permanent: false,
+            destination: '/',
+        },
+    };
+};
+
+export default Redirector;
