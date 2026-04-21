b5ec7e5414864c3424be74fb2316f9ed5b011d80
Gideon Idoko <iamgideonidoko@gmail.com>
2026-04-21 17:54:30 +0100

    chore: make eslint work with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,13 @@
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
 import nextTypeScript from 'eslint-config-next/typescript';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = [
   ...nextCoreWebVitals,
   ...nextTypeScript,
+  prettierRecommended,
   {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      '.eslintcache',
-      'public/**',
-      'next-env.d.ts',
-      'pnpm-lock.yaml',
-    ],
+    ignores: ['node_modules/**', '.next/**', '.eslintcache', 'public/**', 'next-env.d.ts', 'pnpm-lock.yaml'],
   },
 ];
 
